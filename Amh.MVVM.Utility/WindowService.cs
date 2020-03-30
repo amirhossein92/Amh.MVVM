551bae1b78fe908d7acc627229a416ad033b734c
amir hossein <a.mehrabi.70@gmail.com>
2020-03-30 20:06:06 +0430

    refactored window service

--- Amh.MVVM.Utility/WindowService.cs
+++ Amh.MVVM.Utility/WindowService.cs
@@ -20,57 +20,37 @@ namespace Amh.MVVM.Utility
 
         public static void OpenNewPage<T>(ViewModelBase currentViewModel, T viewModel = null) where T : ViewModelBase
         {
-            if (viewModel == null)
-                viewModel = Activator.CreateInstance<T>();
-            if (currentViewModel != null)
-                _mainViewModel.LastUserViewModel = currentViewModel;
-            _mainViewModel.CurrentUserViewModel = viewModel;
+            viewModel = CreateInstance(viewModel);
+            SetCurrentViewModel(currentViewModel, viewModel);
         }
 
-        public static void OpenNewPageWithConstructor<T, Tparameter>(ViewModelBase currentViewModel, Tparameter parameter) where T : ViewModelBase<Tparameter>
+        public static void OpenNewPageWithConstructor<T, Tparameter>(ViewModelBase currentViewModel, Tparameter parameter, T viewModel = null) where T : ViewModelBase<Tparameter>
         {
-            var viewModel = Activator.CreateInstance<T>();
-            if (viewModel is ViewModelBase<Tparameter> viewModelConstructor)
-                viewModelConstructor.Parameter = parameter;
-
-            if (currentViewModel != null)
-                _mainViewModel.LastUserViewModel = currentViewModel;
-            _mainViewModel.CurrentUserViewModel = viewModel;
+            viewModel = CreateInstance(viewModel);
+            PassParameter(viewModel, parameter);
+            SetCurrentViewModel(currentViewModel, viewModel);
         }
 
         public static void OpenNewDialogPage<T>(ViewModelBase currentViewModel, T viewModel = null) where T : ViewModelBase
         {
-            var resources = Application.Current.MainWindow.Resources;
-            var newWindow = new MyDialogWindow();
-            newWindow.SizeToContent = SizeToContent.WidthAndHeight;
-            newWindow.Resources = resources;
-            if (viewModel == null)
-                viewModel = Activator.CreateInstance<T>();
-            newWindow.Title = viewModel.Title;
-            newWindow.Content = viewModel;
+            var newWindow = CreateDialogWindow();
+            viewModel = CreateInstance(viewModel);
+            SetDialogWindowProperties(newWindow, viewModel);
             newWindow.ShowDialog();
         }
 
-        public static void OpenNewPageDialogWithConstructor<T, Tparameter>(ViewModelBase currentViewModel, Tparameter parameter) where T : ViewModelBase<Tparameter>
+        public static void OpenNewPageDialogWithConstructor<T, Tparameter>(ViewModelBase currentViewModel, Tparameter parameter, T viewModel = null) where T : ViewModelBase<Tparameter>
         {
-            var resources = Application.Current.MainWindow.Resources;
-            var newWindow = new MyDialogWindow();
-            newWindow.Resources = resources;
-            newWindow.SizeToContent = SizeToContent.WidthAndHeight;
-            var viewModel = Activator.CreateInstance<T>();
-
-            if (viewModel is ViewModelBase<Tparameter> viewModelConstructor)
-                viewModelConstructor.Parameter = parameter;
-            newWindow.Title = viewModel.Title;
-            newWindow.Content = viewModel;
+            var newWindow = CreateDialogWindow();
+            viewModel = CreateInstance(viewModel);
+            PassParameter(viewModel, parameter);
+            SetDialogWindowProperties(newWindow, viewModel);
             newWindow.ShowDialog();
         }
 
         public static void ClosePage(ViewModelBase currentViewModel)
         {
-            if (currentViewModel != null)
-                _mainViewModel.LastUserViewModel = currentViewModel;
-            _mainViewModel.CurrentUserViewModel = null;
+            SetCurrentViewModel<ViewModelBase>(currentViewModel, null);
         }
 
         public static void CloseDialogPage(ViewModelBase currentViewModel, bool result = false)
@@ -79,6 +59,42 @@ namespace Amh.MVVM.Utility
             var window = windows.OfType<MyDialogWindow>().LastOrDefault();
             if (window != null && window.Content.GetType() == currentViewModel.GetType())
                 window.Close();
+        }
+
+        private static T CreateInstance<T>(T viewModel) where T : ViewModelBase
+        {
+            if (viewModel == null)
+                viewModel = Activator.CreateInstance<T>();
+            return viewModel;
+        }
+
+        private static void SetCurrentViewModel<T>(ViewModelBase currentViewModel, T viewModel) where T : ViewModelBase
+        {
+            if (currentViewModel != null)
+                _mainViewModel.LastUserViewModel = currentViewModel;
+            _mainViewModel.CurrentUserViewModel = viewModel;
+        }
+
+        private static void PassParameter<T, Tparameter>(T viewModel, Tparameter parameter) where T : ViewModelBase<Tparameter>
+        {
+            if (viewModel is ViewModelBase<Tparameter> viewModelConstructor)
+                viewModelConstructor.Parameter = parameter;
+        }
+
+        private static MyDialogWindow CreateDialogWindow()
+        {
+            var resources = Application.Current.MainWindow.Resources;
+            var newWindow = new MyDialogWindow();
+            newWindow.SizeToContent = SizeToContent.WidthAndHeight;
+            newWindow.Resources = resources;
+
+            return newWindow;
+        }
+
+        private static void SetDialogWindowProperties<T>(MyDialogWindow newWindow, T viewModel) where T : ViewModelBase
+        {
+            newWindow.Title = viewModel.Title;
+            newWindow.Content = viewModel;
         }
     }
 }
